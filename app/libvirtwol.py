1666d05025911e52546f94c3d7fbdb1d470b8ca7
realizelol <bsw.bsw@gmx.de>
2023-01-08 00:25:38 +0100

    [libvirtwol] Try sthelse.

--- app/libvirtwol.py
+++ app/libvirtwol.py
@@ -159,17 +159,48 @@ if __name__ == '__main__':
 #    p.setfilter('udp port 9 or ether proto 0x0842', 0, 0)
     netfilter = "udp port 9 or ether proto 0x0842"
 
-    pktlen, timestamp, data = sniff(interface, filters=netfilter, count=-1, promisc=1)
-    logging.info("received[plen]: %s", pktlen)
-    logging.info("received[t]: %s", timestamp)
-    logging.info("received[buf]: %s", data)
-    print("received[plen]: " + pktlen)
-    print("received[t]: " + timestamp)
-    print("received[buf]: " + data)
+    #pktlen, timestamp, data = sniff(interface, filters=netfilter, count=-1, promisc=1)
+    #logging.info("received[plen]: %s", pktlen)
+    #logging.info("received[t]: %s", timestamp)
+    #logging.info("received[buf]: %s", data)
+    #print("received[plen]: " + pktlen)
+    #print("received[t]: " + timestamp)
+    #print("received[buf]: " + data)
     #pdata = pktlen, data, timestamp
+
+    def recv(port=9, addr="192.168.9.246", buf_size=1024):
+        """recv([port[, addr[,buf_size]]]) - waits for a datagram and returns the data."""
+
+        # Create the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # Set some options to make it multicast-friendly
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        except AttributeError:
+                pass # Some systems don't support SO_REUSEPORT
+        s.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_TTL, 20)
+        s.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_LOOP, 1)
+
+        # Bind to the port
+        s.bind(('', port))
+
+        # Set some more multicast options
+        intf = socket.gethostbyname(socket.gethostname())
+        s.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(intf))
+        s.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(addr) + socket.inet_aton(intf))
+
+        # Receive the data, then unregister multicast receive membership, then close the port
+        data, sender_addr = s.recvfrom(buf_size)
+        s.setsockopt(socket.SOL_IP, socket.IP_DROP_MEMBERSHIP, socket.inet_aton(addr) + socket.inet_aton('0.0.0.0'))
+        s.close()
+        return data
+
+    pack = recv()
     while True:
         try:
-            LibVirtWakeOnLan.InspectIPPacket(pktlen, data, timestamp)
+            LibVirtWakeOnLan.InspectIPPacket(pack)
         except KeyboardInterrupt:
             break
         except Exception:
